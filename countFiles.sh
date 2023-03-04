cdd8964bb3a7fc486bbaf30c56fb841ba25175d8
hassan-jsx <qhassan059@gmail.com>
2023-03-05 01:25:01 +0500

    added script file that will display you the number of files of a directory and sub-directory

---
+++ countFiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# define a function to count files with each extension
+function count_files {
+    for file in "$1"/*; do
+        if [ -f "$file" ]; then
+            ext="${file##*.}"
+            ((count["$ext"]++))
+        elif [ -d "$file" ]; then
+            count_files "$file"
+        fi
+    done
+}
+
+# initialize the count array
+declare -A count
+
+# call the count_files function on the current directory
+count_files "."
+
+# print the results
+for ext in "${!count[@]}"; do
+    echo "${ext}: ${count[$ext]}"
+done
